d8491ed25481d06a724ad238c705e77fa45dbd1d
Surajk24 <srjkmr1024@gmail.com>
2020-07-04 20:27:49 -0400

    Added register and login routes for backend

--- functions/index.js
+++ functions/index.js
@@ -26,84 +26,158 @@ const app = express();
 
 const db = admin.firestore();
 
-app.get('/posts', (req, res) => {
-    db.collection('posts').orderBy('createdAt', 'desc').get()
-    .then((data) => {
-        let posts = [];
-        data.forEach((doc) => {
+app.get('/posts', async (req, res) => {
+    try {
+        // get all posts and store them in array
+        let posts= [];
+        const data = await db.collection('posts').orderBy('createdAt', 'desc').get()
+        data.forEach(doc => {
             posts.push({
                 postId: doc.id,
                 ...doc.data()
-            });
-        })
-        return res.json(posts)
-    })
-    .catch(err => console.error(err));
+            })
+        });
+        return res.json(posts);
+    } catch (err) {
+        console.error(err);
+    }
 });
 
-app.post('/posts', (req, res) => {
+// TODO add auth middleware for protected routes
+app.post('/posts', auth, async (req, res) => {
+
+    const { body, userHandle } = req.body;
     const newPost = {
-        body: req.body.body,
-        userHandle: req.body.userHandle,
+        body,
+        userHandle,
         createdAt: new Date().toISOString()
     }
 
-    db.collection('posts').add(newPost)
-    .then((doc) => {
+    try {
+        // create new post
+        const doc = await db.collection('posts').add(newPost);
         res.json({message: `Document ${doc.id} created successfully`});
-    })
-    .catch(err => {
+    } catch (err) {
+        console.error(err);
         res.status(500).json({
             error: 'Server Error'
         });
-        console.error(err);
-    });
+    }
 })
 
+// Method to check for empty strings
+const isEmpty = (string) => {
+    return (string.trim() === '') ? true : false;
+}
+
 //sign up route
-app.post('/register', (req,res) => {
+app.post('/register', async (req,res) => {
+    // Validation errors holder
+    let errors = {}
     const { email, password, confirmPassword, handle } = req.body;
+
     const newUser = {
         email,
-        password,
+        password,         
         confirmPassword,
         handle
     }
 
-    db.doc(`/users/${handle}`).get()
-    .then(doc => {
-       if(doc.exists){
-           return res.status(400).json({
-               handle: 'This handle is already taken'
-           })
-       } else {
-        return firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
-        .then(data => {
-            return data.user.getIdToken()
-            .then(token => {
-                return res.status(201).json({ token })
+    // validate email field
+    if(isEmpty(email)){
+        errors.email = 'Please Enter an email'
+    } 
+    if(isEmpty(password)){
+        errors.password = 'Please Enter a Password'
+    } 
+    if(password !== confirmPassword){
+        errors.confirmPassword = 'Passwords Do not match'
+    }
+    if(isEmpty(handle)){
+        errors.handle = 'Please enter a user handle'
+    }
+
+    // Return in the case of validation errors
+    if(Object.keys(errors).length > 0){
+        return res.status(400).json(errors)
+    }
+ 
+    try {
+        const doc = await db.doc(`/users/${handle}`).get();
+        // If the handle has already been taken
+        if(doc.exists){
+            return res.status(400).json({
+                handle: 'This handle is already taken'
             })
-            .catch(err => {
-                console.error(err);
-                return res.status(500).json({
-                    err: err.code
-                })
-            })
-        })
-        .catch(err => {
+        } else {
+            // Create new user and get token
+            const data = await firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+            const token = await data.user.getIdToken();
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId: data.user.uid
+            }
+            // Creates new user document
+            await db.doc(`/users/${newUser.handle}`).set(userCredentials);
+            return res.status(200).json({ token }); 
+        }  
+    } catch (err) {
+        if(err.code === 'auth/email-already-in-use'){
+            return res.status(400).json({
+                email: 'Email is already in use'
+            });
+        } else {
             console.error(err);
             return res.status(500).json({
-                error: err.code
-            })
-        })
-       }
-    })
-    .catch(err => {
-        return res.status(500).json({
-            error: err.code
-        })
-    })
+                err: err.code
+            }); 
+        }
+    }
 })
+
+// Login route
+app.post('/login', async (req,res) => {
+
+    // Extract body fields
+    const { email, password } = req.body;
+    const user = {
+        email,
+        password
+    }
+
+    let errors = {};
+
+    // Validation for empty fields
+    if(isEmpty(email)){
+        errors.email = 'Please enter your email'
+    }
+    if(isEmpty(password)){
+        errors.password = 'Please enter your password'
+    }
+
+    if(Object.keys(errors).length > 0){
+        return res.status(400).json(errors);
+    }
+
+    try {
+        const data = await firebase.auth().signInWithEmailAndPassword(email, password);
+        const token = await data.user.getIdToken();
+        return res.json({token})   
+    } catch (err) {
+        if(err.code === 'auth/wrong-password'){
+            return res.status(403).json({
+                general: 'Password in incorrect please try again'
+            })
+        } else {
+            console.error(err);
+            res.status(500).json({
+                error: 'Server Error'
+            });   
+        }
+    }
+});
 
 // auto turn the app into base route url/api
 exports.api = functions.https.onRequest(app);
